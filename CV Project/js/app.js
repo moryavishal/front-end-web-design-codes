6d7b06339e86375b0e4c41aeaf284416f11e2dd6
Vishal Morya <86962893+moryavishal@users.noreply.github.com>
2021-10-24 03:12:29 +0530

    Add files via upload

---
+++ CV Project/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+
+// const bars = document.querySelectorAll('.progress_bar');
+
+// bars.forEach(function(bar){
+//    let percentage = bar.dataset.percentage;
+//    let tooltip =bar.children[0];
+//    tooltip.innerText=percentage+'%';
+//    bar.getElementsByClassName.width = percentage +'%';
+//    console.log(percentage);
+// })
+
+});
